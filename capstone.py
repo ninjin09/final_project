75ead940c0c859cd9358a2fe30c370ab1bf55122
Ninjin <d.ninjin@letumongolia.com>
2024-05-10 10:42:02 +0800

    sdfs

--- capstone.py
+++ capstone.py
@@ -143,17 +143,17 @@ if st.button('Give me the latest news!'):
 #     summary = generate_summary(df['article'][0])
 #     st.write(summary)
 
-if st.button('Summarize'):
-    for text in df['article']:
-        auto_abstractor = AutoAbstractor()
-        auto_abstractor.tokenizable_doc = SimpleTokenizer()
-        auto_abstractor.delimiter_list = [".", "\n"]
-        abstractable_doc = TopNRankAbstractor()
+    if st.button('Summarize'):
+        for text in df['article']:
+            auto_abstractor = AutoAbstractor()
+            auto_abstractor.tokenizable_doc = SimpleTokenizer()
+            auto_abstractor.delimiter_list = [".", "\n"]
+            abstractable_doc = TopNRankAbstractor()
+            
+            result_dict = auto_abstractor.summarize(text, abstractable_doc)
         
-        result_dict = auto_abstractor.summarize(text, abstractable_doc)
-    
-        for sentence in result_dict["summarize_result"]:
-            st.write(sentence)
-        
+            for sentence in result_dict["summarize_result"]:
+                st.write(sentence)
+            
 
 
